d0d44ed7833a24fb4e125121d4812cc551f40aa0
IDN <danilstmik14@gmail.com>
2023-12-15 23:19:27 +0700

    migrate

---
+++ public.sql
@@ -0,0 +1,17 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : postgreSQL
+ Source Server Type    : PostgreSQL
+ Source Server Version : 140003
+ Source Host           : localhost:5432
+ Source Catalog        : silat
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 140003
+ File Encoding         : 65001
+
+ Date: 15/12/2023 23:06:42
+*/
+
